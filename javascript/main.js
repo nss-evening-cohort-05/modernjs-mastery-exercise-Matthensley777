90c9a955bab66b8b083458ebc3f8b828874d4b8b
Matt Hensley <matt.hensley777@gmail.com>
2017-05-01 18:00:30 -0500

    adding for loops for each team changing for each

--- javascript/main.js
+++ javascript/main.js
@@ -3,36 +3,42 @@ $(document).ready(function() {
             const characters = [];
             const genders = [];
             const teams = [];
+            const avengers = [];
+            const xMen = [];
+            const guardians = [];
 
-            
-            // const writeToDom = (userSelectedTeam) => {
-            //     const domString = "";
-            //     for(let i = 0; i < characters.length; i++){
-            //         domString += `<p>${characters[i].name}</p>`
-            //         console.log("characters", characters);
+
+            // const myXMen = () => {
+            //     for(let i = 0; i < myXMen.length; i++) {
+            //         if();
             //     }
             // }
 
-// <div class="row">
-//   <div id="rows" class="col-xs-6 col-md-3">
-//     <a href="#" id="charactercards" class="thumbnail">
-//       <img src="..." alt="...">
-//     </a>
-//   </div>
-//   ...
-// </div>
+
+            // const myAvengers = () => {
+            //     for(let i = 0; i < myAvengers.length; i++) {
+            //         if();
+            //     }
+            // }
 
 
+            // const myGuardians = () => {
+            //     for(let i = 0; i < myGuardians.length; i++) {
+            //         if();
+            //     }
+            // }
+
+            
 
 
-
-            // loop for each json file and write to dom function, I think I"m going to re-write this with for loops
-            const writeToDom = (userSelectedTeam) => {
+            // my Write To Dom function 
+            const writeToDom = (superHeroArray) => {
                 const domString = "";
-                let selectedTeam = userSelectedTeam;
+                for(let i = 0; i < superHeroArray.length; i++) {}
+                let selectedTeam = superHeroArray;
                 teams.forEach((currentTeam) => {
-                    domString += `<p>${teams[i].name}</p>`; 
-                    if (currentTeam.name === selectedTeam) {
+                    domString += `<p>${teams.name}</p>`; 
+                    if (currentTeam.name === selectedTeam.id) {
                         characters.forEach((currentCharacters) => {
                             domString += `<div class="row">`;
                             domString += `<div id="rows" class="col-xs-6 col-md-3">;`
@@ -51,8 +57,9 @@ $(document).ready(function() {
                         });
                     }
                 });
-                $("#output").html(domString);
+                $(".output").html(domString);
             };
+
             //ajax and promise for teams
             const teamsJSON = () => {
                 return new Promise((resolve, reject) => {
@@ -91,7 +98,7 @@ $(document).ready(function() {
         .then((results) => {
             console.log(results);
             results.forEach((xhrResult) => {
-                // xhrCall.forEach((characters) => {
+                // xhrCall.forEach((teams) => {
                     characters.push(characters);
 
                 // });
@@ -104,7 +111,7 @@ $(document).ready(function() {
                 return charactersJSON();
             }).then((data2) => {
                 characters = data2;
-                console.log("characters", characters);
+                console.log("characters", data2);
                 return gendersJSON
             }).then((data3) => {
                 genders = data3;
@@ -113,21 +120,22 @@ $(document).ready(function() {
             }).catch((error1) => {
 
             });
-            $("#avengers").click((e) => {
+            $("#avengers").click((a) => {
                 charactersJSON(avengers);
                 console.log("avengers", avengers);
-                writeToDom(e.target.id);
+                writeToDom(a.target.id);
             });
-            $("#xmen").click((e) => {
+            $("#xmen").click((x) => {
                 charactersJSON(xmen);
                 console.log("xmen", xmen);
-                writeToDom(e.target.id);
+                writeToDom(x.target.id);
             });
-            $("#guardians").click((e) => {
+            $("#guardians").click((g) => {
                 charactersJSON(guardians);
                 console.log("guardians", guardians);
-                writeToDom(e.target.id);
+                writeToDom(g.target.id);
             });
+
         });
 
 
