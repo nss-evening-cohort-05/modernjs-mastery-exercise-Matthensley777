f5c2f1a4a9f8c0976ac77c11fc9711efff7724e6
Matt Hensley <matt.hensley777@gmail.com>
2017-05-04 18:04:18 -0500

    changed a lot added for loops for each team

--- javascript/main.js
+++ javascript/main.js
@@ -1,149 +1,140 @@
 $(document).ready(function() {
 
-            const characters = [];
-            const genders = [];
-            const teams = [];
-            const avengers = [];
-            const xMen = [];
-            const guardians = [];
+    let characters = [];
+    let genders = [];
+    let teams = [];
+    let avengers = [];
+    let xMen = [];
+    let guardians = [];
 
 
-            // const myXMen = () => {
-            //     for(let i = 0; i < myXMen.length; i++) {
-            //         if();
-            //     }
-            // }
-
-
-            // const myAvengers = () => {
-            //     for(let i = 0; i < myAvengers.length; i++) {
-            //         if();
-            //     }
-            // }
-
-
-            // const myGuardians = () => {
-            //     for(let i = 0; i < myGuardians.length; i++) {
-            //         if();
-            //     }
-            // }
-
+    // my Write To Dom function 
+    const writeToDom = (superHeroArray) => {
+        let domString = "";
+        for (let g = 0; g < superHeroArray.length; g++) {
+            domString += `<div class="row">`;
+            domString += `<div id="rows" class="col-xs-6 col-md-3">;`
+            domString += `<h4>${superHeroArray[g].name}</h4>`;
+            domString += `<p>${superHeroArray[g].description}</p>`;
+            domString += `<p>${superHeroArray[g].image}</p>`;
+            domString += `</div></div>`
             
 
+        }
 
-            // my Write To Dom function 
-            const writeToDom = (superHeroArray) => {
-                const domString = "";
-                for(let i = 0; i < superHeroArray.length; i++) {}
-                let selectedTeam = superHeroArray;
-                teams.forEach((currentTeam) => {
-                    domString += `<p>${teams.name}</p>`; 
-                    if (currentTeam.name === selectedTeam.id) {
-                        characters.forEach((currentCharacters) => {
-                            domString += `<div class="row">`;
-                            domString += `<div id="rows" class="col-xs-6 col-md-3">;`
-                            domString += `<p>${characters.name}</p>`;
-                            domString += `<p>${characters.description}</p>`;
-                            domString += `<p>${characters.image}</p>`;
-                            domString += `</div></div>`
-                            if (currentCharacters.team_id === currentTeam.id) {
-                                genders.forEach((currentGender) => {
-                                    domString += `<p>${genders.name}</p>`;
-                                    if (currentGender.type === currentCharacters.gender_id) {
+        const myXMen = () => {
+            for (let i = 0; i < myXMen.length; i++) {
+                if (superHeroArray[g].team_id === 0) {
+                    xMen.push(superHeroArray[i]);
+                }
+            }
+            writeToDom(myXMen);
+        }
 
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-                $(".output").html(domString);
-            };
 
-            //ajax and promise for teams
-            const teamsJSON = () => {
-                return new Promise((resolve, reject) => {
-                    $.ajax("./db/teams.json").done((data1) => {
-                        resolve(data1.teams);
-                        console.log(data1);
-                    }).fail((error1) => {
-                        reject(error1);
-                    });
-                });
-            };
-            //ajax and promise for characters
-            const charactersJSON = () => {
-                return new Promise((resolve, reject) => {
-                    $.ajax("./db/characters.json").done((data2) => {
-                        resolve(data2.characters);
-                        console.log(data2);
-                    }).fail((error2) => {
-                        reject(error2);
-                    });
-                });
-            };
-            //ajax and promise for gender
-            const gendersJSON = () => {
-                return new Promise((resolve, reject) => {
-                    $.ajax("./db/genders.json").done((data3) => {
-                        resolve(data3.genders);
-                        console.log(data3);
-                    }).fail((error3) => {
-                        reject(error3);
-                    });
-                });
-            };
+        const myAvengers = () => {
+            for (let j = 0; j < myAvengers.length; j++) {
+                if (superHeroArray[g].team_id === 1) {
+                    avengers.push(superHeroArray[j]);
+                }
+            }
+            writeToDom(myAvengers);
+        }
+
+
+        const myGuardians = () => {
+            for (let k = 0; k < myGuardians.length; l++) {
+                if (superHeroArray[g].team_id === 2) {
+                    guardians.push(superHeroArray[k]);
+                }
+            }
+            writeToDom(myGuardians);
+        }
+        $(".output").html(domString);
+
+    }
+
+
+
+
+
+    //ajax and promise for teams
+    const teamsJSON = () => {
+        return new Promise((resolve, reject) => {
+            $.ajax("./db/teams.json").done((data1) => {
+                resolve(data1.teams);
+                console.log("teamsJSON", data1);
+            }).fail((error1) => {
+                reject(error1);
+            });
+        });
+    };
+    //ajax and promise for characters
+    const charactersJSON = () => {
+        return new Promise((resolve, reject) => {
+            $.ajax("./db/characters.json").done((data2) => {
+                resolve(data2.characters);
+                console.log("charactersJSON", data2);
+            }).fail((error2) => {
+                reject(error2);
+            });
+        });
+    };
+    //ajax and promise for gender
+    const gendersJSON = () => {
+        return new Promise((resolve, reject) => {
+            $.ajax("./db/genders.json").done((data3) => {
+                resolve(data3.genders);
+                console.log("gendersJSON", data3);
+            }).fail((error3) => {
+                reject(error3);
+            });
+        });
+    };
 
     Promise.all([teamsJSON(), charactersJSON(), gendersJSON()])
         .then((results) => {
-            console.log(results);
+            // console.log(results);
             results.forEach((xhrResult) => {
                 // xhrCall.forEach((teams) => {
-                    characters.push(characters);
+                characters.push(characters);
 
-                // });
+                });
             });
-        });
+        // });
 
-            teamsJSON().then((data1) => {
-                teams = data1;
-                // console.log("teams", teams);
-                return charactersJSON();
-            }).then((data2) => {
-                characters = data2;
-                console.log("characters", data2);
-                return gendersJSON
-            }).then((data3) => {
-                genders = data3;
-                console.log(data3);
+    teamsJSON().then((data1) => {
+        teams = data1;
+        // console.log("teams", teams);
+        return charactersJSON();
+    }).then((data2) => {
+        characters = data2;
+        // console.log("characters", data2);
+        return gendersJSON
+    }).then((data3) => {
+        genders = data3;
+        // console.log(data3);
 
-            }).catch((error1) => {
+    }).catch((error1) => {
 
-            });
-            $("#avengers").click((a) => {
-                charactersJSON(avengers);
-                console.log("avengers", avengers);
-                writeToDom(a.target.id);
-            });
-            $("#xmen").click((x) => {
-                charactersJSON(xmen);
-                console.log("xmen", xmen);
-                writeToDom(x.target.id);
-            });
-            $("#guardians").click((g) => {
-                charactersJSON(guardians);
-                console.log("guardians", guardians);
-                writeToDom(g.target.id);
-            });
+    });
+    $("#avengers").click((a) => {
+        charactersJSON(avengers);
+        console.log("avengers", avengers);
+        writeToDom(a.target.id);
+    });
+    $("#xmen").click((x) => {
+        charactersJSON(xMen);
+        console.log("xMen", xMen);
+        writeToDom(x.target.id);
+    });
+    $("#guardians").click((g) => {
+        charactersJSON(guardians);
+        console.log("guardians", guardians);
+        writeToDom(g.target.id);
+    });
 
-        });
-
-
-
-
-
-
-
+});
 
 
 
